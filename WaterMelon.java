f6c62649b08e2f2c666b18e3a2b0455158d1f814
Shreyansh Tripathi <shreyansh.tripathi.02gmail.com>
2021-05-14 17:27:04 +0530

    Problem4A

---
+++ WaterMelon.java
@@ -0,0 +1,22 @@
+/*
+    <a href="https://codeforces.com/problemset/problem/4/A"/>
+*/
+
+import java.util.*;
+
+public class WaterMelon {
+    public static void main(String[] args){
+
+       Scanner sc = new Scanner(System.in);
+       int w = sc.nextInt();
+       if(w>2) {
+           if (w % 2 == 0)
+               System.out.println("YES");
+           else
+               System.out.println("NO");
+       } else {
+           System.out.println("NO");
+       }
+       sc.close();
+    }
+}
